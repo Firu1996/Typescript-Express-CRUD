e845cdc3578341df31a89fd6a602fb031c46e905
Firu1996 <chayakorn.f.ch@gmail.com>
2022-07-08 14:32:24 +0700

    GET employees and GET by Id

--- server/controllers/employee.controller.ts
+++ server/controllers/employee.controller.ts
@@ -2,6 +2,10 @@ import { RequestHandler } from "express";
 import { BaseController } from "./BaseController";
 import employeeModel from "../models/employee.model";
 
+interface IFetchById {
+  id: string;
+}
+
 export class EmployeeController extends BaseController {
   constructor() {
     super();
@@ -20,8 +24,42 @@ export class EmployeeController extends BaseController {
       await newEmployee.save();
 
       this.responseData = { success: true };
-      res.locals = { ...this.responseData, newEmployee };
+      res.locals = { ...this.responseData, data: newEmployee };
       res.status(201).send(res.locals);
+    };
+  }
+
+  fetchEmployee(): RequestHandler {
+    return async (req, res) => {
+      const employeeList = await employeeModel.find();
+
+      this.responseData = { success: true, data: employeeList };
+      res.locals = { ...this.responseData };
+      res.status(200).send(res.locals);
+    };
+  }
+
+  fetchEmployeeById(params: IFetchById): RequestHandler {
+    return async (req, res) => {
+      const { id } = params;
+      if (!id) {
+        res.locals = { ...this.responseData, msg: "Please enter employee id" };
+        res.send(res.locals);
+      }
+
+      const employee = await employeeModel.findById(id);
+      if (employee === null) {
+        this.responseData = { success: true };
+        res.locals = {
+          ...this.responseData,
+          data: employee,
+          msg: "Employee not found",
+        };
+      }
+
+      this.responseData = { success: true };
+      res.locals = { ...this.responseData, data: employee };
+      res.status(200).send(res.locals);
     };
   }
 }
