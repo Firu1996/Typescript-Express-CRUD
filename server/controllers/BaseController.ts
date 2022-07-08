de5621bb8532bfeeeea943b6e8a7abbae035b792
Firu1996 <chayakorn.f.ch@gmail.com>
2022-07-08 14:18:38 +0700

    Create BaseController.ts

---
+++ server/controllers/BaseController.ts
@@ -0,0 +1,10 @@
+import { Request } from "express";
+
+interface IBodyResponse {
+  success: boolean;
+  data?: any;
+}
+
+export abstract class BaseController {
+  protected responseData: IBodyResponse = { success: true };
+}
