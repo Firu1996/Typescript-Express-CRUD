8ce140825a9e41641bb054cb68e848909dbb183e
Firu1996 <chayakorn.f.ch@gmail.com>
2022-07-08 14:32:09 +0700

    GET fetchEmployee

--- server/routes/employee.route.ts
+++ server/routes/employee.route.ts
@@ -6,3 +6,4 @@ export const employeeRouter = Router();
 const employeeController = new EmployeeController();
 
 employeeRouter.post("/", employeeController.createEmployee());
+employeeRouter.get("/", employeeController.fetchEmployee());
